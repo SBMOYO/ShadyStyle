14cda2c0069599c19c24407099bbf07ba0589a43
SBMOYO <sunganomoyo1@gmail.com>
2023-10-04 15:00:18 +0200

    first commit

---
+++ ShadyStyle/store/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from .models import Sunglass
+
+# Create your views here.
+def index(request):
+    sunglass = Sunglass.objects.all()
+    return render(request, 'index.html',{'sunglass': sunglass})
+
+def shop(request):
+    apply_position_head = True
+    return render(request, 'shop.html', {'apply_position_head': apply_position_head})
+
+def about(request):
+    apply_position_head = True
+    return render(request, 'about.html',{'apply_position_head': apply_position_head})
+
+def glasses(request):
+    apply_position_head = True
+    sunglass = Sunglass.objects.all()
+    return render(request, 'glasses.html', {'sunglass': sunglass, 'apply_position_head': apply_position_head})
+
+def contact(request):
+    apply_position_head = True
+    return render(request, 'contact.html', {'apply_position_head': apply_position_head})

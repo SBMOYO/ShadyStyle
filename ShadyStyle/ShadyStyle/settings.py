d8a686161564e0b279dcf1c64d9051c61b493b3c
SBMOYO <sunganomoyo1@gmail.com>
2024-02-11 12:09:29 +0200

    changed host to localhost

--- ShadyStyle/ShadyStyle/settings.py
+++ ShadyStyle/ShadyStyle/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': 'db',
         'USER': 'sungano',
         'PASSWORD': '19981123',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }

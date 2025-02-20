9270afd5358d35a483e922c42cd178d1f2e276f8
SBMOYO <sunganomoyo1@gmail.com>
2025-02-20 11:56:52 +0200

    database roll up

--- ShadyStyle/ShadyStyle/settings.py
+++ ShadyStyle/ShadyStyle/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-jk%zxbsmj_m&hsuto7w-!eyyzv^c18)3&qudbuf&x0f6y*^ag*'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-jk%zxbsmj_m&hsuto7w-!eyyzv^c18)3&qudbuf&x0f6y*^ag*'
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ.get('DEBUG')
+DEBUG = True
+# DEBUG = os.environ.get('DEBUG')
 
 # ALLOWED_HOSTS = ["*"]
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'ShadyStyle.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgresql://shadystyle_database_jm7s_user:UOhRbajF0S5oKZVqZ4jVWoJeDVVtBcoi@dpg-cs38551u0jms7396hbkg-a/shadystyle_database_jm7s')
+    'default': dj_database_url.config(default='postgresql://shadystyle_database_cpod_user:KblO1MzQw92OPTKmuzmasbLZI0AwpiHi@dpg-curfipa3esus73dnmsu0-a/shadystyle_database_cpod')
 }
 
 # Password validation
